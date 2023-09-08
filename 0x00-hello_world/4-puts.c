f59bf8bb827b5dbd49eed914922ca443a2191b0d
Eric Anane <anane2020@gmail.com>
2023-09-07 22:10:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a text followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
