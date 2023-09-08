3a794987e583af0f6fb41136ba5ed347204beb34
Eric Anane <anane2020@gmail.com>
2023-09-07 22:29:38 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C Prints a text followed by a new line using printf funtion
+ * Return: 0
+ */
+int main(void)
+{
+pintf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
